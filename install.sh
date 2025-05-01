1e31e6199b112969c8a8006c0b53756b0b745508
ademlo1545bobby <yadem3259@gmail.com>
2025-05-01 10:25:03 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,7 +11,7 @@ sleep 1
 echo -e "\e[93m→ Installing required packages...\e[0m"
 pkg update -y
 pkg upgrade -y
-pkg install git curl wget python -y
+pkg install curl git wget python jq termux-api -y
 
 # Clone the project from GitHub
 echo -e "\e[92m→ Downloading project from GitHub...\e[0m"
@@ -19,8 +19,8 @@ git clone https://github.com/ademlo1545bobby/adem1545
 
 # Enter the directory and give execution permission
 cd adem1545
-chmod +x adem1545.sh
+chmod +x install.sh
 
 # Launch the main script
 echo -e "\e[94m→ Starting ADEM1545...\e[0m"
-bash adem1545.sh
+bash install.sh
